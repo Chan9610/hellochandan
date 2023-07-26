8ae4c068389bb82df46c4a3eb98c3dfecbd22ecf
Chan9610 <clakhara34@gmail.com>
2023-07-26 20:21:48 +0530

    first

---
+++ index.js
@@ -0,0 +1,66 @@
+// ===========================================================================================
+
+// let b="Hello World How Are You"
+// document.write(b.length)   
+// ===========================================================================================
+
+// *** String Length=> It will count the length of string.***
+// let b="Hello World How Are You"
+// ===========================================================================================
+
+// document.write(b.toLocaleUpperCase()) 
+// ***To Upper Case is a function which make all the Element of string Upper Case***
+// ===========================================================================================
+
+// let b="Hello World How Are You"
+// document.write(b.toLocaleLowerCase()) 
+// ***To Lower Case is a function which make all the Element of string Lower ***
+// ===========================================================================================
+
+// let b="Hello World, How Are You"
+// document.write(b.replace("You","Chandan"))
+// ***string replace***
+// ===========================================================================================
+
+// let b="Hello World, How Are You"
+// document.write(b.replaceAll("e","E"))
+// ***String RepalceAll will used to replace every single element in the sentence*** 
+// ===========================================================================================
+
+// let b="Hello World, How Are You"
+// let c="  I'm Good"
+// document.write(b.concat(c))
+// *** String Concat make two seperate string a Single String***
+// ===========================================================================================
+
+// let b="               Hello World, How Are You"
+// console.log(b.trimStart())
+// ***Trimstart will Remove all the space from starting pragraph***
+// ===========================================================================================
+
+// let b="Hello World, How Are You                         "
+// console.log(b.trimEnd())
+// ***TrimEnd will Reove all the Space from end of sentence***
+// ===========================================================================================
+
+// let b="Hello World, How Are You"
+// console.log(b.charAt(6))
+// ***charAt will find the string word with the source of index value***
+// ===========================================================================================
+
+// let b="Hello World, How Are You"
+//    console.log(b.charCodeAt(8))
+//***charCodeAt Return the unicode of the character with index number***
+// ===========================================================================================
+
+// let b="Hello World,How Are You"
+//     console.log(b.slice(0,7))
+
+// ***Slice will take a slipe of words from a sentence of string***    
+// ===========================================================================================
+
+
+// let b="Hello World,How Are You"
+//     console.log(b.search("o"))
+// *** Search Method of string it will search the element gives and gives index number of string***
+// ===========================================================================================
